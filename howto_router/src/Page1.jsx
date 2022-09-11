8e34e4ceb5fb4d95ec61dc9d9c5d6fe0816c0b0c
jun <junpeeei0407@gmail.com>
2022-09-11 15:59:47 +0900

    Router終了

--- howto_router/src/Page1.jsx
+++ howto_router/src/Page1.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export const Page1 = () => {
+
+    const navigate = useNavigate();
+    const onClickDetailA = () => navigate('detailA', { state: arr })
+
+    const arr = [...Array(100).keys()];
     return (
         <div>
             <h1>Page1ページです</h1>
-            <Link to="/page1/detailA">DetailaA</Link>
+            <Link to="/page1/detailA" state={arr}>DetailaA</Link>
             <br />
             <Link to="/page1/detailB">DetailaB</Link>
+            <br />
+            <button onClick={onClickDetailA}>DetailA</button>
         </div>
     )
 }
--- howto_router/src/router/Router.jsx
+++ howto_router/src/router/Router.jsx
@@ -5,15 +5,20 @@ import { Page1 } from '../Page1'
 import { Page1DetailA } from '../Page1DetailA'
 import { Page1DetailB } from '../Page1DetailB'
 import { Page2 } from '../Page2'
+import { Page404 } from '../Page404'
+import { UrlParameter } from '../UrlParameter'
 
 export const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/page1" element={<Page1 />} />
-            <Route path="page1/DetailA" element={<Page1DetailA />} />
-            <Route path="page1/DetailB" element={<Page1DetailB />} />
+            <Route path="/page1/DetailA" element={<Page1DetailA />} />
+            <Route path="/page1/DetailB" element={<Page1DetailB />} />
+
             <Route path="/page2" element={<Page2 />} />
+            <Route path="/page2/:id" element={<UrlParameter />} />
+            <Route path="*" element={<Page404 />} />
         </Routes>
 
     )
