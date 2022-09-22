f76aebca9c466b4f03011ff7b7f0ad5d0c548712
jun <junpeeei0407@gmail.com>
2022-09-23 00:47:26 +0900

    design

--- usersapp/src/conponents/pages/Home.tsx
+++ usersapp/src/conponents/pages/Home.tsx
@@ -1,8 +1,11 @@
+import { useAuth } from "../../hooks/useAuth"
 import { HeaderLayout } from "../templates/HeaderLayout"
 
 export const Home = () => {
+    const { error } = useAuth();
     return (
         <>
+            { }
             <HeaderLayout />
             <p>HOME画面です</p>
         </>
